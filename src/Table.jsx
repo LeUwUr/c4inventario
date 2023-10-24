10817290e0a09ad9290b69b98e7ec26417e27a94
LeUwUr <evadevizcarra@gmail.com>
2023-10-24 16:48:53 -0700

    Añadida la segunda parte del diseño de la página principal de inventario, con la incorporación de iconos importados desde React Icons.

--- src/Table.jsx
+++ src/Table.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { CgAdd } from 'react-icons/cg';
+import { AiFillBell } from 'react-icons/ai';
+import { BiSolidUser } from 'react-icons/bi';
+import { RxExit } from 'react-icons/rx';
 
 function InventoryTable() {
   const inventoryData = [
@@ -6,36 +10,54 @@ function InventoryTable() {
       UPC: "84998006979",
       NOMBRE: "Escritorio de metal",
       UBICACION: "Recepcion",
-      CREADO_POR: "Eva Aguirre"
+      CREADO_POR: "Eva Aguirre",
     },
     {
       UPC: "819215021232",
       NOMBRE: "Lampara de metal",
       UBICACION: "Recepcion",
-      CREADO_POR: "Eva Aguirre"
+      CREADO_POR: "Eva Aguirre",
     },
     {
       UPC: "683609866729",
       NOMBRE: "Bote de basura",
       UBICACION: "Sala de juntas",
-      CREADO_POR: "Howard Garcia"
+      CREADO_POR: "Howard Garcia",
     },
     {
       UPC: "683609866729",
       NOMBRE: "Bote de basura",
       UBICACION: "Sala de juntas",
-      CREADO_POR: "Howard Garcia"
+      CREADO_POR: "Howard Garcia",
     }
     // ... add other items here
   ];
 
   return (
-    
-    <div className="w-full px-20 py-2">
+    <div className="w-full py-0">
+      {/* Navbar */}
+      <nav className="bg-red-900 p-2 mb-5 text-center w-screen">
+        <div className="flex items-center justify-between"> {/* Added justify-between */}
+        <div className="flex justify-center items-center w-full ml-28">
+            <input
+              type="text"
+              placeholder="Buscar..."
+              className="bg-white p-2 rounded-full w-8/12"
+            />
+          </div>
+          <div className="flex items-center space-x-6 mr-20">
+            <CgAdd alt='Agregar' className="text-white w-10 h-10" />
+            <AiFillBell alt='Notificaciones' className="text-white w-10 h-10" />
+            <BiSolidUser alt='Usuario' className="text-white w-10 h-10" />
+            <RxExit alt='Cerrar sesion' className="text-white w-10 h-10" />
+          </div>
+        </div>
+      </nav>
+
       <h1 className="text-2xl font-bold text-red-900 mb-5 text-center">
         Inventario
-        </h1>
-      <table className="mx-auto min-w-full text-center bg-white">
+      </h1>
+      <table className="mx-auto w-10/12 text-center bg-white">
         <thead>
           <tr>
             <th className="py-2 px-4 border text-white bg-amber-500">UPC</th>
@@ -62,15 +84,12 @@ function InventoryTable() {
                 <button className="bg-lime-600 text-white px-4 py-2 mr-2 rounded">
                   Detalles
                 </button>{" "}
-                {/* Marked change */}
                 <button className="bg-sky-400 text-white px-4 py-2 mr-2 rounded">
                   Actualizar
                 </button>{" "}
-                {/* Marked change */}
                 <button className="bg-red-800 text-white px-4 py-2 rounded">
                   Baja
                 </button>{" "}
-                {/* Marked change */}
               </td>
             </tr>
           ))}
